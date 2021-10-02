5b8d7a1f49e8969f7731d35b4956622f2cac2771
Newt <dragonminer101@gmail.com>
2021-10-02 14:55:15 +0100

    adding extension credits

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -12,5 +12,13 @@ return [
     //     'version' => '1.0.0',
     // ],
     
+    'extension_tracker' => [
+        'key' => 'shop_features',
+        'wiki_key' => 'Shop_Features',
+        'creators' => json_encode([
+            'Newt' => 'https://github.com/ne-wt/',
+        ]),
+        'version' => '1.2.0',
+    ],
 
 ];
