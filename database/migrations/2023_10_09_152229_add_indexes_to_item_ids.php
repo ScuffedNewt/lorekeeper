4080186f2b01ca98e16c5bae4d44d4b5bccdb607
Ne-wt <Ne-wt@users.noreply.github.com>
2023-10-09 15:26:10 +0000

    refactor: fix PHP styling

--- database/migrations/2023_10_09_152229_add_indexes_to_item_ids.php
+++ database/migrations/2023_10_09_152229_add_indexes_to_item_ids.php
@@ -4,15 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::table('items', function (Blueprint $table) {
             //
             $table->index('item_category_id');
@@ -21,11 +17,8 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::table('items', function (Blueprint $table) {
             //
             $table->dropIndex('items_item_category_id_index');
