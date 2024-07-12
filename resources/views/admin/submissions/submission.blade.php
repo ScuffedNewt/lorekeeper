30e9f5be3ee306a9a6d6edef27eb7178c4560b78
ScuffedNewt <dragonminer101@gmail.com>
2024-07-12 21:21:12 +0100

    fix: put elements inside widget

--- resources/views/admin/submissions/submission.blade.php
+++ resources/views/admin/submissions/submission.blade.php
@@ -90,8 +90,7 @@
                 </div>
             @endif
             @foreach ($submission->characters()->whereRelation('character', 'deleted_at', null)->get() as $character)
-                'elements' => $elements,
-                @include('widgets._character_select_entry', ['characterCurrencies' => $characterCurrencies, 'items' => $items, 'tables' => $tables, 'character' => $character, 'expanded_rewards' => $expanded_rewards])
+                @include('widgets._character_select_entry', ['characterCurrencies' => $characterCurrencies, 'items' => $items, 'tables' => $tables, 'character' => $character, 'expanded_rewards' => $expanded_rewards, 'elements' => $elements])
             @endforeach
         </div>
         <div class="text-right mb-3">
