31e4a84061287062c04972a1a111f6d6a061e144
ScuffedNewt <ScuffedNewt@users.noreply.github.com>
2024-07-20 21:44:47 +0000

    refactor: fix blade formatting

--- resources/views/widgets/_add_limits.blade.php
+++ resources/views/widgets/_add_limits.blade.php
@@ -2,7 +2,7 @@
     // $limits = \App\Models\Limit\DynamicLimit::all();
 
     // map the keys and the 'name' value of config('lorekeeper.limits.limit_types')
-    $limitTypes = collect(config('lorekeeper.limits.limit_types'))->map(function($value, $key) {
+    $limitTypes = collect(config('lorekeeper.limits.limit_types'))->map(function ($value, $key) {
         return $value['name'];
     });
     $limits = \App\Models\Limit\Limit::hasLimits($object) ? \App\Models\Limit\Limit::getLimits($object) : null;
@@ -26,53 +26,53 @@
     {!! isset($info) ? '<p class="alert alert-info">' . $info . '</p>' : '' !!}
 
     {!! Form::open(['url' => 'admin/limits']) !!}
-        {!! Form::hidden('object_model', get_class($object)) !!}
-        {!! Form::hidden('object_id', $object->id) !!}
-        <div class="limit">
-            <div id="limits">
-                @if ($limits)
-                    <h5>Limits for {!! $limits->first()->object->displayName !!}</h5>
-                    @foreach ($limits as $limit)
-                        <div class="row">
-                            <div class="col-md-3 form-group">
-                                {!! Form::label('Limit Type') !!}
-                                {!! Form::select('limit_type[]', $limitTypes, $limit->limit_type, ['class' => 'form-control limit-selectize limit-type', 'placeholder' => 'Select Limit Type']) !!}
-                            </div>
-                            <div class="col-md-4 form-group limit-select">
-                                {!! Form::label('limit_id', 'Limit') !!}
-                                @if ($limit->limit_type == 'prompt')
-                                    {!! Form::select('limit_id[]', $prompts, $limit->limit_id, ['class' => 'form-control limit prompts', 'placeholder' => 'Enter Limit']) !!}
-                                @elseif ($limit->limit_type == 'item')
-                                    {!! Form::select('limit_id[]', $items, $limit->limit_id, ['class' => 'form-control limit items', 'placeholder' => 'Enter Limit']) !!}
-                                @elseif ($limit->limit_type == 'currency')
-                                    {!! Form::select('limit_id[]', $currencies, $limit->limit_id, ['class' => 'form-control limit currencies', 'placeholder' => 'Enter Limit']) !!}
-                                @elseif ($limit->limit_type == 'dynamic')
-                                    {!! Form::select('limit_id[]', $dynamics, $limit->limit_id, ['class' => 'form-control limit dynamics', 'placeholder' => 'Enter Limit']) !!}
-                                @endif
-                            </div>
-                            <div class="col-md-4 quantity">
-                                <div class="form-group">
-                                    {!! Form::label('Quantity') !!}
-                                    {!! Form::number('quantity[]', $limit->quantity, ['class' => 'form-control', 'placeholder' => 'Enter Quantity', 'min' => 0, 'step' => 1]) !!}
-                                </div>
-                                @if ($limit->limit_type == 'currency' || $limit->limit_type == 'item')
-                                    <div class="form-group">
-                                        {!! Form::label('Debit') !!}
-                                        {!! Form::select('debit[]', ['yes' => 'Debit', 'no' => 'Don\'t Debit'], $limit->debit ? 'yes' : 'no', ['class' => 'form-control']) !!}
-                                    </div>
-                                @endif
-                            </div>
-                            <div class="col-md-1 d-flex align-items-center">
-                                <div class="btn btn-danger remove-limit mx-auto">X</div>
-                            </div>
+    {!! Form::hidden('object_model', get_class($object)) !!}
+    {!! Form::hidden('object_id', $object->id) !!}
+    <div class="limit">
+        <div id="limits">
+            @if ($limits)
+                <h5>Limits for {!! $limits->first()->object->displayName !!}</h5>
+                @foreach ($limits as $limit)
+                    <div class="row">
+                        <div class="col-md-3 form-group">
+                            {!! Form::label('Limit Type') !!}
+                            {!! Form::select('limit_type[]', $limitTypes, $limit->limit_type, ['class' => 'form-control limit-selectize limit-type', 'placeholder' => 'Select Limit Type']) !!}
                         </div>
-                    @endforeach
-                @endif
-            </div>
-            <div class="btn btn-secondary" id="add-limit">Add Limit</div>
-            <i class="fas fa-trash text-danger float-right mt-2 mx-2 fa-2x" data-toggle="tooltip" title="To delete limits, simply remove all existing limits and click 'Edit Limits'"></i>
-            {!! Form::submit(($limits ? 'Edit' : 'Create') . ' Limits', ['class' => 'btn btn-primary float-right']) !!}
+                        <div class="col-md-4 form-group limit-select">
+                            {!! Form::label('limit_id', 'Limit') !!}
+                            @if ($limit->limit_type == 'prompt')
+                                {!! Form::select('limit_id[]', $prompts, $limit->limit_id, ['class' => 'form-control limit prompts', 'placeholder' => 'Enter Limit']) !!}
+                            @elseif ($limit->limit_type == 'item')
+                                {!! Form::select('limit_id[]', $items, $limit->limit_id, ['class' => 'form-control limit items', 'placeholder' => 'Enter Limit']) !!}
+                            @elseif ($limit->limit_type == 'currency')
+                                {!! Form::select('limit_id[]', $currencies, $limit->limit_id, ['class' => 'form-control limit currencies', 'placeholder' => 'Enter Limit']) !!}
+                            @elseif ($limit->limit_type == 'dynamic')
+                                {!! Form::select('limit_id[]', $dynamics, $limit->limit_id, ['class' => 'form-control limit dynamics', 'placeholder' => 'Enter Limit']) !!}
+                            @endif
+                        </div>
+                        <div class="col-md-4 quantity">
+                            <div class="form-group">
+                                {!! Form::label('Quantity') !!}
+                                {!! Form::number('quantity[]', $limit->quantity, ['class' => 'form-control', 'placeholder' => 'Enter Quantity', 'min' => 0, 'step' => 1]) !!}
+                            </div>
+                            @if ($limit->limit_type == 'currency' || $limit->limit_type == 'item')
+                                <div class="form-group">
+                                    {!! Form::label('Debit') !!}
+                                    {!! Form::select('debit[]', ['yes' => 'Debit', 'no' => 'Don\'t Debit'], $limit->debit ? 'yes' : 'no', ['class' => 'form-control']) !!}
+                                </div>
+                            @endif
+                        </div>
+                        <div class="col-md-1 d-flex align-items-center">
+                            <div class="btn btn-danger remove-limit mx-auto">X</div>
+                        </div>
+                    </div>
+                @endforeach
+            @endif
         </div>
+        <div class="btn btn-secondary" id="add-limit">Add Limit</div>
+        <i class="fas fa-trash text-danger float-right mt-2 mx-2 fa-2x" data-toggle="tooltip" title="To delete limits, simply remove all existing limits and click 'Edit Limits'"></i>
+        {!! Form::submit(($limits ? 'Edit' : 'Create') . ' Limits', ['class' => 'btn btn-primary float-right']) !!}
+    </div>
     {!! Form::close() !!}
 </div>
 
