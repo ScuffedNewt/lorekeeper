2037b64bc782a3b64a00d8334819b29054e89016
Uri <45401276+preimpression@users.noreply.github.com>
2024-09-16 12:40:09 -0400

    fix glitchy pet drops
    I reviewed my code and this line seems to??? simply cause some pets to not want to ever drop. Looks like it was an erroneous hold-over from the myo slot bit on character drops. Removed this bit on my own site and those bugged pets can now drop

--- app/Models/Pet/PetDrop.php
+++ app/Models/Pet/PetDrop.php
@@ -65,7 +65,7 @@ class PetDrop extends Model {
      * @return \Illuminate\Database\Eloquent\Builder
      */
     public function scopeRequiresUpdate($query) {
-        return $query->whereNotIn('user_pet_id', UserPet::pluck('pet_id')->toArray())->whereIn('drop_id', PetDropData::where('is_active', 1)->pluck('id')->toArray())->where('next_day', '<', Carbon::now());
+        return $query->whereIn('drop_id', PetDropData::where('is_active', 1)->pluck('id')->toArray())->where('next_day', '<', Carbon::now());
     }
 
     /**********************************************************************************************
