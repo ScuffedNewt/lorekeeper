7bb59c582e709e381ead4b4d42fbecac5824adf8
jam <ch3rvbcreations@gmail.com>
2024-10-14 14:43:30 -0400

    e

--- resources/views/widgets/_reward_display.blade.php
+++ resources/views/widgets/_reward_display.blade.php
@@ -1,5 +1,5 @@
 @php
-    if (!$earner) {
+    if (!isset($earner)) {
         $earner = 'User';
         $rewardses = $object->objectRewards;
     } elseif ($earner == 'Character') {
@@ -7,26 +7,30 @@
     }
 @endphp
 <hr class="my-4 w-75" />
+
 <h4>{{ ucfirst($type) }} Rewards ({!! $earner == 'User' ? 'User <i class="fas fa-user"></i>' : 'Character <i class="fas fa-paw"></i>' !!} )</h4>
 
 @if (isset($info))
     <div class="alert alert-info">{!! $info !!}</div>
 @endif
-
-<table class="table table-sm">
-    <thead>
-        <tr>
-            <th width="70%">Reward</th>
-            <th width="30%">Amount</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($rewardses as $reward)
+@if ($rewardses->count())
+    <table class="table table-sm">
+        <thead>
             <tr>
-                <td>{!! $reward->reward->displayName !!}</td>
-                <td>{{ $reward->quantity }}</td>
+                <th width="70%">Reward</th>
+                <th width="30%">Amount</th>
             </tr>
-        @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @foreach ($rewardses as $reward)
+                <tr>
+                    <td>{!! $reward->reward->displayName !!}</td>
+                    <td>{{ $reward->quantity }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@else
+    <p>No rewards.</p>
+@endif
 <hr class="my-4 w-75" />
