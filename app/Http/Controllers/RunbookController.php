86b347348c5e064cffbec411090a37b183adee41
ScuffedNewt <ScuffedNewt@users.noreply.github.com>
2026-01-05 23:58:46 +0000

    refactor: fix PHP styling

--- app/Http/Controllers/RunbookController.php
+++ app/Http/Controllers/RunbookController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Runbook;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class RunbookController extends Controller {
     /*
