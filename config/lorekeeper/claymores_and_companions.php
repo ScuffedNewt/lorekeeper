9e6df70dff926dc3b5142685d6e891bdc806fa37
ScuffedNewt <scuffednewt@gmail.com>
2026-03-01 21:14:15 +0000

    fix: config defaults

--- config/lorekeeper/claymores_and_companions.php
+++ config/lorekeeper/claymores_and_companions.php
@@ -11,16 +11,16 @@ return [
     ],
 
     'stat_points' => [
-        'general_id' => 2, // the ID of the general stat point that is used for leveling up characters and users
+        'general_id' => null, // the ID of the general stat point that is used for leveling up characters and users
     ],
 
     'visibility_settings' => [
-        'user_levels'       => 1,
-        'character_levels'  => 1,
-        'character_classes' => 1,
-        'character_skills'  => 1,
-        'character_stats'   => 1,
-        'weapons'           => 1,
-        'gear'              => 1,
+        'user_levels'       => 0,
+        'character_levels'  => 0,
+        'character_classes' => 0,
+        'character_skills'  => 0,
+        'character_stats'   => 0,
+        'weapons'           => 0,
+        'gear'              => 0,
     ],
 ];
