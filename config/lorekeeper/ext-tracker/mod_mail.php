309048d77585e65cb68ba34b9bcc5c4eaa8aba15
ScuffedNewt <scuffednewt@gmail.com>
2026-07-07 19:54:28 +0100

    feat(credits): Update credits

---
+++ config/lorekeeper/ext-tracker/mod_mail.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'wiki_key' => 'mod_mail',
+    'creators' => json_encode([
+        'ScuffedNewt'     => 'https://github.com/ScuffedNewt/',
+        'AnimatedCritter' => 'https://github.com/AnimatedCritter/',
+    ]),
+    'version'  => '1.0.0',
+];
