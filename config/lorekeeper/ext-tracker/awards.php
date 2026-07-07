1e5e189f60d51da0c837cc91387af676d792a664
ScuffedNewt <scuffednewt@gmail.com>
2026-07-07 19:32:30 +0100

    feat(credits): Update credits

--- config/lorekeeper/ext-tracker/awards.php
+++ config/lorekeeper/ext-tracker/awards.php
@@ -3,9 +3,9 @@
 return [
     'wiki_key' => 'Awards',
     'creators' => json_encode([
-        'Uri'   => 'https://github.com/preimpression/',
-        'TGI'   => 'https://github.com/tjgallaha/',
-        'Ne-wt' => 'https://github.com/ne-wt/',
+        'Uri'         => 'https://github.com/preimpression/',
+        'TGI'         => 'https://github.com/tjgallaha/',
+        'ScuffedNewt' => 'https://github.com/ScuffedNewt/',
     ]),
     'version' => '2.0.0',
 ];
