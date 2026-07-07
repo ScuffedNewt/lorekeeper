761a21a602f4fd25d706046dedf13e8ff63fd941
ScuffedNewt <ScuffedNewt@users.noreply.github.com>
2026-07-07 18:58:41 +0000

    refactor: fix PHP styling

--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -1,6 +1,7 @@
 <?php
+
 return [
-    
+
     // FORMAT EXAMPLE.
     //
     // 'extension_tracker' => [
@@ -11,9 +12,9 @@ return [
     //     ]),
     //     'version' => '1.0.0',
     // ],
-    
+
     'mini_clock' => [
-        'key' => 'mini_clock',
+        'key'      => 'mini_clock',
         'wiki_key' => 'Mini_Clock',
         'creators' => json_encode([
             'Newt' => 'https://github.com/ne-wt/',
