0a30c356083379231581aafa403302318f85c738
ScuffedNewt <scuffednewt@gmail.com>
2026-07-07 19:41:26 +0100

    feat(credits): Update credits

--- config/lorekeeper/ext-tracker/elemental_typing.php
+++ config/lorekeeper/ext-tracker/elemental_typing.php
@@ -3,7 +3,7 @@
 return [
     'wiki_key' => 'Elemental_Typing',
     'creators' => json_encode([
-        'Newt' => 'https://github.com/ne-wt/',
+        'ScuffedNewt' => 'https://github.com/ScuffedNewt/',
     ]),
-    'version'  => '1.0.0',
+    'version'  => '1.0.1',
 ];
