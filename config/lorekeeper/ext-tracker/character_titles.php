f262c0f4b340541c552fe2e3640f0373150314cb
ScuffedNewt <scuffednewt@gmail.com>
2026-07-07 18:46:42 +0100

    feat(credits): Update credits

---
+++ config/lorekeeper/ext-tracker/character_titles.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'key'      => 'character_titles',
+    'wiki_key' => 'Character_Titles',
+    'creators' => json_encode([
+        'Mercury' => 'https://github.com/itinerare/',
+        'ScuffedNewt' => 'https://github.com/ScuffedNewt/'
+    ]),
+    'version' => '1.1.6',
+];
--- config/lorekeeper/extension_tracker.php
+++ config/lorekeeper/extension_tracker.php
@@ -18,6 +18,7 @@ return [
         'wiki_key' => 'Character_Titles',
         'creators' => json_encode([
             'Mercury' => 'https://github.com/itinerare/',
+            'ScuffedNewt' => 'https://github.com/ScuffedNewt/'
         ]),
         'version' => '1.1.6',
     ],
