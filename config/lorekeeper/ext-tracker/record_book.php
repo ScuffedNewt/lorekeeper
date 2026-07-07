fea70be359d55d2daba6d6fce17ea5d654f1df3b
ScuffedNewt <scuffednewt@gmail.com>
2026-07-07 20:03:46 +0100

    feat(credits): Update credits

---
+++ config/lorekeeper/ext-tracker/record_book.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'wiki_key' => 'Record_Book',
+    'creators' => json_encode([
+        'ScuffedNewt' => 'https://github.com/ScuffedNewt/',
+    ]),
+    'version'  => '1.0.0',
+];
