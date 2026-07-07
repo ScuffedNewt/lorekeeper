c09754844baad3a102a4bd99509746f376bfa631
ScuffedNewt <scuffednewt@gmail.com>
2026-07-07 18:51:23 +0100

    feat(credits): Update credits
    - Removed shop feature credits because it's merged into develop

---
+++ config/lorekeeper/ext-tracker/pets.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'wiki_key' => 'Pets',
+    'creators' => json_encode([
+        'ScuffedNewt' => 'https://github.com/ScuffedNewt/',
+        'Mercury'     => 'https://github.com/itinerare/',
+        'Uri'         => 'https://github.com/preimpression/',
+        'SUPERCOOL'   => 'https://github.com/CH3RVB/',
+        'Perappu'     => 'https://github.com/Perappu/',
+    ]),
+    'version' => '2.0.1',
+];
